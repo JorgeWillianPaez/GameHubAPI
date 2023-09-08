4b36496f3579b06a5b91a2f68597490ead5adce6
Lucas Purkota <lucaspurkota@gmail.com>
2023-09-07 23:05:43 -0300

    Alterand classes Amizade, Biblioteca, Compra e Usuario

--- GameHubAPI/Models/BibliotecaModel.cs
+++ GameHubAPI/Models/BibliotecaModel.cs
@@ -2,5 +2,30 @@
 {
     public class BibliotecaModel
     {
+        private int id;
+        private string nome;
+        private string descricao;
+        private UsuarioModel usuario;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+        public string Nome
+        {
+            get => nome;
+            set => nome = value;
+        }
+        public string Descricao
+        {
+            get => descricao;
+            set => descricao = value;
+        }
+        public UsuarioModel Usuario
+        {
+            get => usuario;
+            set => usuario = value;
+        }
     }
 }
--- GameHubAPI/Models/UsuarioModel.cs
+++ GameHubAPI/Models/UsuarioModel.cs
@@ -1,6 +1,57 @@
-﻿namespace GameHubAPI.Models
+﻿using System.Net.Mail;
+
+namespace GameHubAPI.Models
 {
     public class UsuarioModel
     {
+        private int id;
+        private string nome;
+        private string nickName;
+        private string email;
+        private DateOnly dataNascimento;
+        private string senha;
+        private string status;
+        private string usuarioAtivado;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+        public string Nome
+        {
+            get => nome;
+            set => nome = value;
+        }
+        public string NickName
+        {
+            get => nickName;
+            set => nickName = value;
+        }
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+        public DateOnly DataNascimento
+        {
+            get => dataNascimento;
+            set => dataNascimento = value;
+        }
+        public string Senha
+        {
+            get => senha;
+            set => senha = value;
+        }
+        public string Status
+        {
+            get => status;
+            set => status = value;
+        }
+        public string UsuarioAtivado
+        {
+            get => usuarioAtivado;
+            set => usuarioAtivado = value;
+        }
     }
 }
